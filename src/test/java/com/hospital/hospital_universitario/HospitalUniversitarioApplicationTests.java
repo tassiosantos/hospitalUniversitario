c49b09b9c9cebc3ee94f5eeadc40995d81f08e90
Tássio Nascimento Santos <tassiosantos@gmail.com>
2021-07-21 01:56:42 -0300

    iniciando projeto

---
+++ src/test/java/com/hospital/hospital_universitario/HospitalUniversitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_universitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalUniversitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
