98a83862532cc846fbb67654c5016645f3d85fb4
João Vitor <vorque45@gmail.com>
2021-07-24 11:26:29 -0300

    retirei importaçoes nao utilizadas

--- src/main/java/com/hospital/hospital_universitario/controllers/LaudoController.java
+++ src/main/java/com/hospital/hospital_universitario/controllers/LaudoController.java
@@ -6,12 +6,12 @@ import com.hospital.hospital_universitario.models.Laudo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,12 +34,12 @@ public class LaudoController{
     }
 
 
-	@PutMapping(path = "/laudo/{laudoNumber}")
-	public Laudo updateLaudo(@PathVariable("laudoNumber") String laudoNumber){
-		//Laudo laudo = this.laudoService.update(laudoNumber);
-		//return laudo;
-        return null;
-	}
+	// @PutMapping(path = "/laudo/{laudoNumber}")
+	// public Laudo updateLaudo(@PathVariable("laudoNumber") String laudoNumber){
+	// 	//Laudo laudo = this.laudoService.update(laudoNumber);
+	// 	//return laudo;
+    //     return null;
+	// }
 
 	// @PostMapping(path = "/laudo")
 	// public void addAccount(@RequestBody Account newAccount){
