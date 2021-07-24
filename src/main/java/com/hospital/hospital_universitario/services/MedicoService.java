3ce83603b2b38e1e9fce54e079656f351dfc07cc
João Vitor <vorque45@gmail.com>
2021-07-24 10:33:32 -0300

    adicionei o serviço medico

---
+++ src/main/java/com/hospital/hospital_universitario/services/MedicoService.java
@@ -0,0 +1,26 @@
+package com.hospital.hospital_universitario.services;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hospital.hospital_universitario.models.Medico;
+import com.hospital.hospital_universitario.repositories.MedicoRepository;
+
+
+@Service
+public class MedicoService {
+    
+    private final MedicoRepository MedicoRepository;
+
+    @Autowired
+    public MedicoService(MedicoRepository medicoRepository){
+        this.medicoRepository = medicoRepository;
+    }
+
+    public List<Medico> getLaudos(){
+        List<Medico> medicos = medicoRepository.findAll();
+        return medicos;
+    }
+}
