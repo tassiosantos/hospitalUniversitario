844fd1a5ca4f33b2152f4b76776f9396ff53e303
João Vitor <vorque45@gmail.com>
2021-07-24 11:31:31 -0300

    adicionei o repositorio de solicitaçao de exames

---
+++ src/main/java/com/hospital/hospital_universitario/repositories/SolicitacaoExameRepository.java
@@ -0,0 +1,14 @@
+package com.hospital.hospital_universitario.repositories;
+
+import java.util.List;
+
+import com.hospital.hospital_universitario.models.SolicitacaoExame;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SolicitacaoExameRepository extends JpaRepository<SolicitacaoExame, Long>{
+    List<SolicitacaoExame> findById(int id);
+
+}
