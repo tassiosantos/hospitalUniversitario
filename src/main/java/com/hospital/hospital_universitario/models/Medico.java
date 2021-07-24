eb8453d6c11f198eb6450054bfbfcc9f04320001
João Vitor <vorque45@gmail.com>
2021-07-24 10:15:49 -0300

    adicionei o model medico

---
+++ src/main/java/com/hospital/hospital_universitario/models/Medico.java
@@ -0,0 +1,68 @@
+package com.hospital.hospital_universitario.models;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Medico {
+	
+	@Id
+    @SequenceGenerator(
+        name = "medico_sequence",
+        sequenceName = "medico_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.AUTO,
+        generator = "medico_sequence"
+    )
+	
+	private int Id;
+	private String nome;
+	private String cpf;
+	//Por enquanto
+	private String CRM;
+	private String titulacao;
+	//Tem que criar um enum para a variável embaixo
+	private String tipo;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public void setCRM(String CRM) {
+		this.CRM = CRM;
+	}
+	public String getCRM() {
+		return CRM;
+	}
+	public void setTitulacao(String titulacao) {
+		this.titulacao = titulacao;
+	}
+	public String getTitulacao() {
+		return titulacao;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	
+}
