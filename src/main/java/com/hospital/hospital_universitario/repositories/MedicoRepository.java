418bde3f191e296db6a41c630bbf55a3a95b9753
João Vitor <vorque45@gmail.com>
2021-07-26 23:14:41 -0300

    o retorno por id é de somente um médico e não uma lista deles

--- src/main/java/com/hospital/hospital_universitario/repositories/MedicoRepository.java
+++ src/main/java/com/hospital/hospital_universitario/repositories/MedicoRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
-    List<Medico> findById(int id);
+   Medico findById(int id);
 
 }
