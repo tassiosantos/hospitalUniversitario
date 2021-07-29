ec064ec2f86ed12a879873807c382b00c475a269
João Vitor <vorque45@gmail.com>
2021-07-28 22:51:03 -0300

    adicionei métodos ao paciente service

--- src/main/java/com/hospital/hospital_universitario/repositories/PacienteRepository.java
+++ src/main/java/com/hospital/hospital_universitario/repositories/PacienteRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
-    List<Paciente> findById(int id);
+    Paciente findById(int id);
 
 }
