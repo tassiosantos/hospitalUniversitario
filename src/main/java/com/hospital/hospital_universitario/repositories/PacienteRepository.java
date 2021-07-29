a0182784584dcd85ee8b9edd7cb4060e9be4103d
João Vitor <vorque45@gmail.com>
2021-07-28 22:51:55 -0300

    removi importação não utilizada

--- src/main/java/com/hospital/hospital_universitario/repositories/PacienteRepository.java
+++ src/main/java/com/hospital/hospital_universitario/repositories/PacienteRepository.java
@@ -1,7 +1,5 @@
 package com.hospital.hospital_universitario.repositories;
 
-import java.util.List;
-
 import com.hospital.hospital_universitario.models.Paciente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
