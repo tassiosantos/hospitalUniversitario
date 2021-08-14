e053f7c5eb36f62593502b568b6cf3c4349e60e5
Giovane-F-Moreira <giovane.fern.m@gmail.com>
2021-08-13 21:32:49 -0300

    Adicionado 3 dependencias ao projeto para trabalhar com template do thymeleaf, utilizando bootstrap junto a ele, foi feito o front do cadastrar paciente 'index' e o cadastrar medico 'fazer rota no controler para o cadastrar medico'

--- src/main/java/com/hospital/hospital_universitario/controllers/indexController.java
+++ src/main/java/com/hospital/hospital_universitario/controllers/indexController.java
@@ -1,13 +1,21 @@
 package com.hospital.hospital_universitario.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class indexController {
 	
-	@RequestMapping("/")
-	public String index() {
+	@GetMapping("/")    
+    public String index() {
+        return "index";   
+    }
+
+    @RequestMapping(value="/cadastrarPaciente", method=RequestMethod.GET)
+	public String form() {
 		return "index";
 	}
+
 }
