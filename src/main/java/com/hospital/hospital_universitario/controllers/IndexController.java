b91aec2ced76dc4a24d61801f7715d6449a83832
João Vitor <vorque45@gmail.com>
2021-08-28 22:28:42 -0300

    adicionei uma rota para o login do usuario

--- src/main/java/com/hospital/hospital_universitario/controllers/IndexController.java
+++ src/main/java/com/hospital/hospital_universitario/controllers/IndexController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 	
 	@GetMapping("/")    
-    public String index() {
-        return "index";   
+    public String login() {
+        return "login";   
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 
     // @GetMapping("/laudo")
