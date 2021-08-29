df9b616270daa10c4f21c4e7210635335b45827c
João Vitor <vorque45@gmail.com>
2021-08-29 17:42:10 -0300

    .

--- src/main/java/com/hospital/hospital_universitario/repositories/MedicoRepository.java
+++ src/main/java/com/hospital/hospital_universitario/repositories/MedicoRepository.java
@@ -10,9 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
    Medico findById(int id);
-<<<<<<< HEAD
-=======
    Medico findByCrmAndSenha(String crm, String senha);
 
->>>>>>> f3f253b6cad0c1b71806ccbb39c19b8ec5cd5171
 }
