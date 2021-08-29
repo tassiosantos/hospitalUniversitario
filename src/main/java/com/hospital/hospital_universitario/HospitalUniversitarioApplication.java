1a769c26d99e5194ce3c287bdb4f937c5f3b30c3
Tássio Nascimento Santos <tassiosantos@gmail.com>
2021-08-29 11:11:05 -0300

    autenticação

--- src/main/java/com/hospital/hospital_universitario/HospitalUniversitarioApplication.java
+++ src/main/java/com/hospital/hospital_universitario/HospitalUniversitarioApplication.java
@@ -2,12 +2,31 @@ package com.hospital.hospital_universitario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class HospitalUniversitarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalUniversitarioApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String teste() {
+		return "index";
+	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 
 }
--- src/main/java/com/hospital/hospital_universitario/controllers/IndexController.java
+++ src/main/java/com/hospital/hospital_universitario/controllers/IndexController.java
@@ -8,26 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 	
-	@GetMapping("/")    
-    public String index() {
-        return "index";   
-    }
+	// @GetMapping("/")    
+    // public String index() {
+    //     return "index";   
+    // }
 
     @GetMapping("/laudo")
     public String laudo() {
         return "./laudo/Cadastrar_Laudo";
     }
     
-    // @GetMapping("/medico")
-    // public String medico() {
-    //     return "./medico/Cadastrar_Medico";
-    // }
-    
-    // @GetMapping("/paciente")
-    // public String paciente() {
-    //     return "./paciente/Listar_Paciente";
-    // }    
-
     @GetMapping("/exame")
     public String exame() {
         return "./exame/Cadastrar_Exame";
